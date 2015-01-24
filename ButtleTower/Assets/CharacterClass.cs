311f1ec44022147580dc56488a67b7cddbb5f13f
mtdt7 <tannenbaum.max@gmail.com>
2015-01-24 14:42:33 -0500

    adding weapon and character classes

---
+++ ButtleTower/Assets/CharacterClass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterClass {
+
+	public int initHealth = 100;
+	public int armor = 0;
+	public int damageTaken = 0;
+	public int health
+	{
+		get{ return (initHealth + armor*50) - damageTaken;}
+	}
+	public bool inv = false;
+	public WeaponClass weapon = null;
+	public bool isDead
+	{
+		get { return (health <= 0);}
+	}
+}
