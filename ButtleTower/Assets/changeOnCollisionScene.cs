2947ce9a94652ebe0f00a50447636c046ef7e2f2
Jonathan Go <jongo593@gmail.com>
2015-01-24 16:30:35 -0500

    Added scene changing scripts

---
+++ ButtleTower/Assets/changeOnCollisionScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeOnCollisionScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		print ("Loaded: " + gamestate.Instance.getLevel());
+	}
+
+
+
+
+	void OnTriggerEnter (Collider other)
+	{    
+		Debug.Log ("OnTriggerEnter : other.tag = " + other.tag); // shows the tag of the trigger
+		// if tag is door
+		if (other.tag == "Player")
+		{
+			int curLevel = gamestate.Instance.getLevel();
+			print ("Collision! Moving to level "+(curLevel+1).ToString());
+			gamestate.Instance.setLevel(curLevel+1);
+			Application.LoadLevel ("level"+(curLevel+1).ToString()); 
+		}
+	}
+}
---
+++ ButtleTower/Assets/levelgui.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelgui : MonoBehaviour {
+
+	// Initialize level
+	void Start () 
+	{
+		print ("Loaded: " + gamestate.Instance.getLevel());
+	}
+	
+	
+	
+	// ---------------------------------------------------------------------------------------------------
+	// OnGUI()
+	// --------------------------------------------------------------------------------------------------- 
+	// Provides a GUI on level scenes
+	// ---------------------------------------------------------------------------------------------------
+	void OnGUI()
+	{	
+		{
+			int curLevel = gamestate.Instance.getLevel();
+			// Create buttons to move between level 1 and level 2
+			if (GUI.Button (new Rect (30, 30, 150, 30), "Load Level 1"))
+			{
+				print ("Moving to level "+curLevel+1.ToString());
+				gamestate.Instance.setLevel(curLevel+1);
+				Application.LoadLevel("level"+(curLevel+1));
+			}
+			
+			if (GUI.Button (new Rect (300, 30, 150, 30), "Load Level 2"))
+			{
+				print ("Moving to level "+curLevel+1.ToString());
+				gamestate.Instance.setLevel(curLevel+1);
+				Application.LoadLevel("level"+(curLevel+1));
+			}		
+			
+			// Output stats
+			GUI.Label(new Rect(30, 100, 400, 30), "Name: " + gamestate.Instance.getName());
+			GUI.Label(new Rect(30, 130, 400, 30), "HP: " + gamestate.Instance.getHP().ToString());
+			GUI.Label(new Rect(30, 160, 400, 30), "MP: " + gamestate.Instance.getMP().ToString());
+		
+		}
+	}
+}
